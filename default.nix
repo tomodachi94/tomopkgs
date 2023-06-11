258f01a812479cebe9f8e857252f31c1cbc28363
Tomo <68489118+Tomodachi94@users.noreply.github.com>
2023-06-11 14:57:52 -0700

    spyglass: init

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  spyglass = pkgs.callPackage ./pkgs/spyglass { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
